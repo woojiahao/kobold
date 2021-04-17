5bc979600605c10478205d81c1400b079df3ef3e
woojiahao <woojiahao1234@gmail.com>
2021-04-17 22:49:55 +0800

    Add constraint for url format

--- priv/repo/migrations/20210417123254_create_url_table.exs
+++ priv/repo/migrations/20210417123254_create_url_table.exs
@@ -10,6 +10,11 @@ defmodule Kobold.Repo.Migrations.CreateUrlTable do
       add :user_id, references(:user, type: :uuid, column: :user_id)
     end
 
+    create constraint(
+      :url,
+      :url_format,
+      check: "original ~ 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'"
+    )
     create index(:url, [:user_id])
   end
 end
