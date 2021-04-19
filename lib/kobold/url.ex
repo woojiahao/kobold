4d0e4840890b894836dad1e9f2f8146bd655158b
woojiahao <woojiahao1234@gmail.com>
2021-04-19 21:33:56 +0800

    Create insert url func for proper validated insert

--- lib/kobold/url.ex
+++ lib/kobold/url.ex
@@ -18,11 +18,38 @@ defmodule Kobold.Url do
     )
   end
 
-  def changeset(data, params \\ %{}) do
-    data
-    |> cast(params, [:expiration_date])
-    |> validate_required([:hash, :original, :creation_date])
-    |> validate_length(:original, max: 512)
-    |> foreign_key_constraint(:user_id)
+  @url_regex ~r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,255}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+
+  def insert(hash, original, creation_date, expiration_date \\ nil, user_id \\ nil) do
+    params = %{
+      hash: hash,
+      original: original,
+      creation_date: creation_date,
+      expiration_date: expiration_date
+    }
+
+    url =
+      %Kobold.Url{}
+      |> cast(params, [:hash, :original, :creation_date, :expiration_date])
+      |> validate_required([:hash, :original, :creation_date])
+      |> validate_length(:original, max: 512)
+      |> validate_format(:original, @url_regex, message: "invalid URL format")
+      |> foreign_key_constraint(:user_id)
+
+    if user_id do
+      user =
+        Kobold.User
+        |> Kobold.Repo.get!(user_id)
+        |> Kobold.Repo.preload(:urls)
+
+      url =
+        url
+        |> change()
+        |> put_assoc(:user, user)
+
+      Kobold.Repo.insert!(url)
+    else
+      Kobold.Repo.insert!(url)
+    end
   end
 end
