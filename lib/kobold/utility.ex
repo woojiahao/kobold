38bea8b8f1775d837c7a5cd1784f90a9967a2093
woojiahao <woojiahao1234@gmail.com>
2021-04-24 18:58:15 +0800

    Add shuffle random function
    Extract randomly shuffled string from string

---
+++ lib/kobold/utility.ex
@@ -0,0 +1,39 @@
+defmodule Kobold.Utility do
+  @spec shuffle_random(String.t(), number()) :: {:ok, String.t()} | {:error, String.t()}
+  def shuffle_random(_block, length) when length <= 0,
+    do: {:error, "invalid length provided, provide at least 1 character"}
+
+  def shuffle_random(block, length) do
+    str_len = String.length(block)
+
+    if length > str_len do
+      {:error, "length of shuffled string cannot be greater than block"}
+    else
+      indices = generate_indices(str_len, length, [])
+
+      if indices |> Enum.any?(&(&1 >= str_len)) do
+        {:error, "invalid index generated"}
+      else
+        shuffled = indices |> Enum.map(&String.at(block, &1)) |> Enum.join()
+        {:ok, shuffled}
+      end
+    end
+  end
+
+  @spec append(list(), any()) :: list()
+  def append(list, value), do: [value | list |> Enum.reverse()] |> Enum.reverse()
+
+  # Generates a list of random indices based on given size, counting up to max
+  defp generate_indices(_max, size, indices) when length(indices) == size, do: indices
+
+  defp generate_indices(max, size, indices) do
+    # Not too sure the benefits of using this over Enum.random
+    index = :rand.uniform(max) - 1
+
+    if index in indices do
+      generate_indices(max, size, indices)
+    else
+      generate_indices(max, size, append(indices, index))
+    end
+  end
+end
---
+++ test/kobold/utility_test.exs
@@ -0,0 +1,35 @@
+defmodule Kobold.UtilityTest do
+  use ExUnit.Case
+  import Kobold.Utility
+
+  describe "testing shuffle_random" do
+    setup do
+      # Ensures that the first random number is always the same
+      :rand.seed(:exro928ss, 1..16 |> Enum.to_list() |> Enum.map(&((&1 + 15) * 17 - 11)))
+      []
+    end
+
+    test "length cannot be < 0" do
+      {status, _} = shuffle_random("foo", -1)
+      assert status == :error
+    end
+
+    test "length cannot be == 0" do
+      {status, _} = shuffle_random("foo", 0)
+      assert status == :error
+    end
+
+    test "length cannot be > input string length" do
+      {status, _} = shuffle_random("foo", 4)
+      assert status = :error
+    end
+
+    test "shuffle_random('abc123', 5) shuffles to 'ac312'" do
+      assert shuffle_random("abc123", 5) == {:ok, "ac312"}
+    end
+
+    test "shuffle_random('s58bo7l3', 6) shuffles to 's837b5'" do
+      assert shuffle_random("s58bo7l3", 6) == {:ok, "s837b5"}
+    end
+  end
+end
