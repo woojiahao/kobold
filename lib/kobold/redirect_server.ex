08d55dc9d720b738d102189368642039d382fe32
woojiahao <woojiahao1234@gmail.com>
2021-04-25 10:11:49 +0800

    Add basic redirect functionality when GET path

--- lib/kobold/redirect_server.ex
+++ lib/kobold/redirect_server.ex
@@ -1,14 +1,36 @@
 defmodule Kobold.RedirectServer do
   use Plug.Router
 
+  if Mix.env() == :dev do
+    use Plug.Debugger
+  end
+
+  plug(Plug.Logger)
   plug(:match)
   plug(:dispatch)
 
-  get "/hello" do
-    send_resp(conn, 200, "world")
+  get "/:path" do
+    # TODO: Add caching check
+
+    if path == "favicon.ico" do
+      conn |> resp(200, "") |> put_resp_header("Content-Type", "image/x-icon")
+    else
+      url = Kobold.Url.get(path)
+      conn |> resp(:found, "") |> put_resp_header("location", url.original)
+    end
+  end
+
+  post "/create" do
+    # TODO: Create a hashed URL
+    send_resp(conn, 200, "Creating URL")
+  end
+
+  delete "/delete/:hash" do
+    # TODO: Restrict this to user only keys
+    send_resp(conn, 200, "Deleting key")
   end
 
   match _ do
-    send_resp(conn, 308, "something")
+    send_resp(conn, 404, "Invalid path")
   end
 end
